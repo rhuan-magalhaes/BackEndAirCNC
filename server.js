58939075bef4e27a450473a4a362e34c146f43da
RHUAN MAGALHAES ASSOLA <rhuan.massola@senacsp.edu.br>
2025-05-29 19:41:03 -0300

    Atualização completa do Back

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config(); 
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+
+const routes = require('./routes');
+
+const app = express();
+// Habilita o parser de JSON em todas as rotas
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    return res.send('API AirCNC Rodando ...')
+})
+
+//Rota de teste rápida
+app.get('/ping', (req, res) => {
+  console.log('🔔 recebeu ping');
+  res.send('pong');
+});
+
+// Registra as rotas da aplicação
+// - a partir deste ponto, qualquer requisição irá passar pelo seu router (routes.js)
+// Por que o app.use(routes) aqui?
+// Deve vir após o express.json(), para que o body já seja convertido em objeto JavaScript.
+// Registra todas as sub-rotas definidas em routes.js.
+app.use(routes)
+
+async function startDatabase(){
+   
+    const { DB_USER, DB_PASS, DB_NAME } = process.env;
+
+    const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@clusterapi.j79s3.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=ClusterAPI`;
+    // await mongoose.connect('mongodb+srv://login:senha@omnistack9.qwkirlk.mongodb.net/dataArcnc?retryWrites=true&w=majority&appName=omnistack9');
+    // mongodb+srv://rhuanmagalhaes0801:<db_password>@clusterapi.j79s3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAPI
+
+    try {
+        await mongoose.connect(uri);
+        console.log('Conectado ao MongoDBAtlas')
+    } catch (error) {
+        console.error(' Erro ao conectar ao MongoDB: ', error.message);
+        process.exit(1); // Encerra o processo se a conexão falhar
+    }
+}
+
+startDatabase().then( () => {
+    
+    const port = process.env.PORT || 3335
+    const server = app.listen(port, () => {
+        console.log(`Servidor rodando na porta ${port}`)
+    })
+
+})
+
